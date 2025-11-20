3a2ab4300bb65e1a8fe8695c90291e6441e693be
leizongbless <2215817102@qq.com>
2025-11-20 11:32:35 +0800

    add shell for infer

--- infer_kinedex.sh
+++ infer_kinedex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash#!/bin/bash
-python infer_forcemimic.py \
-  --config-name train_forcemimic_image_workspace \
-  task=forcemimic \
-  task.name=forcemimic_infer \
+python infer_kinedex.py \
+  --config-name train_kinedex_image_workspace \
+  task=kinedex \
+  task.name=kinedex_infer \
   logging.mode=offline \
   load_pca_path=/home/robotics/Prometheus/reactive_diffusion_policy/tactile_pca/board \
   load_ckpt_path=/home/robotics/Tactile-Baseline/ckpt/latest_dp_bb.ckpt
