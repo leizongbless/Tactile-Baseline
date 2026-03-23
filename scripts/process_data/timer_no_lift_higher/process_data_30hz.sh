999d1f8c9c7b54fcf5bbc926199f2abdebd2c006
Zhijie Yan <yanzhijie@buaa.edu.cn>
2026-03-23 17:16:18 +0800

    add scripts for processing timer_no_lift_higher

---
+++ scripts/process_data/timer_no_lift_higher/process_data_30hz.sh
@@ -0,0 +1,13 @@
+# end_frame多截取5frame，而vase_sponge_test1多截取20frame。因为vase_sponge_test1最后是抬高，而end_frame根据x轴来判断。所以vase_sponge_test1需要多截取
+python scripts/process_data/timer/process_data_all_zarr_any_frequency.py \
+    --root_path /home/tars/projects/force_data \
+    --save_path /home/tars/projects/visual_tactile_policy/kywang/processed_timer_no_lift_higher/processed_30hz \
+    --task_list jishiqi_0323_80 \
+    --episode_length 80 \
+    --target_fps 30 \
+    --start_z_diff_thresh 0.1 \
+    --end_extra_frames 0 \
+    # --save_camera_vis \
+    # --episode_length 2
+
+# /home/tars/projects/force_data/jishiqi_0318_80
