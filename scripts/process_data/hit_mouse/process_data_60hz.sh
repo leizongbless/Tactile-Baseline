47d3f17b71027385b7e901b9c7ed493f67c2b348
Zhijie Yan <yanzhijie@buaa.edu.cn>
2026-03-30 16:40:14 +0800

    add scripts for process hit_mouse_0330_50

--- scripts/process_data/hit_mouse/process_data_60hz.sh
+++ scripts/process_data/hit_mouse/process_data_60hz.sh
@@ -1,8 +1,8 @@
 # end_frame多截取5frame，而vase_sponge_test1多截取20frame。因为vase_sponge_test1最后是抬高，而end_frame根据x轴来判断。所以vase_sponge_test1需要多截取
 python scripts/process_data/hit_mouse/process_data_all_zarr_any_frequency.py \
     --root_path /home/tars/projects/force_data \
-    --save_path /home/tars/projects/visual_tactile_policy/kywang/processed_hit_mouse/processed_60hz \
-    --task_list dishu_0324_50 \
+    --save_path /home/tars/projects/visual_tactile_policy/kywang/processed_hit_mouse_0330_50/processed_60hz \
+    --task_list dishu_0330_50 \
     --episode_length 50 \
     --target_fps 60 \
     --start_z_diff_thresh 0.05 \
