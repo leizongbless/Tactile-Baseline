f0af9e7ee4525994b357882907a79689ca7e6111
sadpiggy <wkykaixin@sjtu.edu.cn>
2026-04-02 02:33:13 +0000

    add scripts for training dp/ldp with kernel3 on hit_mouse_merge_0330_0324

---
+++ scripts/train/rdp_ldp/hit_mouse_merge_0330_0324/train_rdp_ldp_60hz_kernel3_horizon48.sh
@@ -0,0 +1,44 @@
+# #!/bin/bash
+# 5hour = 3600
+# echo "sleep for 18000"
+# sleep 18200
+
+GPU_ID=3
+
+horizon=48
+
+TASK_NAME="wipe"
+# Point to the dataset directory that contains 'replay_buffer.zarr'
+# DATASET_PATH="/data/kywang/projects/tactile_il/data/processed/vase_new_C/rdp_zarr"
+DATASET_PATH="/home/kywang/projects/efficient_robot_sys/data/ckpts/hit_mouse_merge_0330_0324_60hz/rdp_zarr"
+LOGGING_MODE="online"
+TIMESTAMP=hit_mouse_merge_ldp_horizon${horizon}
+SEARCH_PATH="./data/outputs"
+num_epochs=450 
+kernel_size=3
+
+
+AT_LOAD_DIR="/home/kywang/projects/efficient_robot_sys/data/ckpts/hit_mouse_merge_0330_0324_60hz/ckpts_abs/rdp_vae/n_embed_10/horizon48/latest.ckpt"
+# AT_LOAD_DIR="data/outputs/2026.03.24/08.11.32_train_vae_real_wipe_hit_mouse_merge_0330_0324_rdp_vae_60hz_horizon48/checkpoints/latest.ckpt"
+
+# # # Stage 2: Train Latent Diffusion Policy
+echo ""
+echo "Stage 2: training Latent Diffusion Policy..."
+CUDA_VISIBLE_DEVICES=${GPU_ID} accelerate launch train.py \
+    --config-name=train_latent_diffusion_unet_real_image_workspace \
+    policy.kernel_size=${kernel_size} \
+    policy.noise_scheduler.num_train_timesteps=30 \
+    policy.num_inference_steps=30 \
+    task=real_${TASK_NAME}_image_gelsight_emb_ldp_24fps_without_tactile \
+    task.dataset_path=${DATASET_PATH} \
+    task.dataset.relative_action=False \
+    task.name=real_${TASK_NAME}_ldp_kernel${kernel_size}_${TIMESTAMP} \
+    at=at_wipe_lift \
+    at_load_dir=${AT_LOAD_DIR} \
+    logging.mode=${LOGGING_MODE} \
+    at.dataset_obs_temporal_downsample_ratio=1 \
+    at.horizon=${horizon} \
+    at.n_obs_steps=1 \
+    at.policy.use_rnn_decoder=False \
+    at.policy.n_embed=10 \
+    training.num_epochs=${num_epochs}
