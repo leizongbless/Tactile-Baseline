b660c62570d9a72681d585d89c31a7bc66b5a69a
sadpiggy <wkykaixin@sjtu.edu.cn>
2026-04-05 09:12:02 +0000

    add scripts for training hit_mouse_merge_0402

---
+++ scripts/train/rdp_vae/n_embed_10/train/hit_mouse_merge_0402_0330_0234/train_rdp_vae_60hz_horizon48.sh
@@ -0,0 +1,32 @@
+# #!/bin/bash
+
+GPU_ID=0
+horizon=48
+
+TASK_NAME="wipe"
+# Point to the dataset directory that contains 'replay_buffer.zarr'
+# DATASET_PATH="/data/kywang/projects/tactile_il/data/processed/vase_new_C/rdp_zarr"
+DATASET_PATH="/home/kywang/projects/efficient_robot_sys/data/ckpts/hit_mouse_merge_0402_0330_0324_60hz/rdp_zarr"
+LOGGING_MODE="online"
+TIMESTAMP=hit_mouse_merge_0402_0330_0324_rdp_vae_60hz_horizon${horizon}
+SEARCH_PATH="./data/outputs"
+num_epochs=600
+
+# Stage 1: Train Asymmetric Tokenizer
+echo "Stage 1: training Asymmetric Tokenizer..."
+CUDA_VISIBLE_DEVICES=${GPU_ID} python train.py \
+    --config-name=train_at_workspace \
+    task=real_${TASK_NAME}_image_gelsight_emb_at_24fps \
+    task.dataset_path=${DATASET_PATH} \
+    task.dataset.relative_action=False \
+    task.name=${TIMESTAMP} \
+    at=at_wipe_lift \
+    logging.mode=${LOGGING_MODE} \
+    at.dataset_obs_temporal_downsample_ratio=1 \
+    at.horizon=${horizon} \
+    at.n_obs_steps=1 \
+    at.policy.use_rnn_decoder=False \
+    at.policy.n_embed=10 \
+    training.num_epochs=${num_epochs} \
+    # dataloader.batch_size=128 \
+    # dataloader.num_workers=1
