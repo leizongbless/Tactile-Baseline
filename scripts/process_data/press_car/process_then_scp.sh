71352a9fa29208491beb9ee6d903165867a69d08
Zhijie Yan <yanzhijie@buaa.edu.cn>
2026-04-08 20:31:09 +0800

    add scripts for process car data

--- scripts/process_data/press_car/process_then_scp.sh
+++ scripts/process_data/press_car/process_then_scp.sh
@@ -4,6 +4,6 @@
 echo "process 60hz"
 # bash scripts/process_data/press_car_0407/process_data_60hz.sh
 
-rsync -avzP -e "ssh -p 1030" \
-    /home/tars/projects/visual_tactile_policy/kywang/processed_press_car_0407 \
-    root@8.130.212.67:/mnt/data/kywang/visual_tactile/dataset/
+rsync -avzP -e "ssh -p 22" \
+    /home/tars/projects/visual_tactile_policy/kywang/processed_press_car_0407_250 \
+    root@10.10.54.14:/mnt/data/kywang/visual_tactile/dataset/
